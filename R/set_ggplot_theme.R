3ccb3fe5b941e6f9bced8b898bdff05d11f0e018
elahi <robin.elahi@gmail.com>
2024-02-06 10:39:16 -0800

    updated site with caballes

---
+++ R/set_ggplot_theme.R
@@ -0,0 +1,7 @@
+#' Robin Elahi
+#' ggplot settings
+
+library(tidyverse)
+theme_set(theme_bw(base_size = 12) + 
+            theme(panel.grid = element_blank(), 
+                  strip.background = element_blank()))
